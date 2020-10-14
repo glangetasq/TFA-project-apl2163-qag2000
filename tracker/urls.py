92d4ce28555974c37a29f078dd93044d537cb901
Quentin Glangetas <qag2000@columbia.edu>
2020-10-14 17:11:26 -0400

    Added update view.

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,9 +1,10 @@
 
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'tracker'
 
 urlpatterns = [
     path('sightings/', views.index, name='index'),
+    re_path(r'sightings/(?P<primary_key>[\w-]+)', views.update, name='update')
 ]
