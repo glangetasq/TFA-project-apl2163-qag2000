c2e64375285ad459fce96e78ea0461dee954c271
Quentin Glangetas <qag2000@columbia.edu>
2020-10-14 17:21:58 -0400

    Fixed a path problem in url

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,10 +1,10 @@
 
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 app_name = 'tracker'
 
 urlpatterns = [
     path('sightings/', views.index, name='index'),
-    re_path(r'sightings/(?P<primary_key>[\w-]+)', views.update, name='update')
+    path('sightings/<str:squirrel_id>', views.update, name='update')
 ]
