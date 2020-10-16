f1390e3e7e8b14932edb6deb0cab183f43f358f6
Quentin Glangetas <qag2000@columbia.edu>
2020-10-16 13:39:17 -0400

    Launch.sh launch server from sratch now.

--- launch.sh
+++ launch.sh
@@ -1,1 +1,8 @@
+python -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py import_squirrel_data Data/squirrel_sightings.csv
 sudo /home/qag2000/squirrel_tracker/env/bin/python manage.py runserver 0.0.0.0:80
+
