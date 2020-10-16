c474c8a793c263cc201ed611d9fe739ef8468528
Quentin Glangetas <qag2000@columbia.edu>
2020-10-16 13:42:27 -0400

    Dummy change to overwrite general_stats.py

--- Stats/general_stats.py
+++ Stats/general_stats.py
@@ -2,19 +2,25 @@ from django.db.models import Count
 
 def general_stats(squirrel_list):
 
-    TotalNumber = len(squirrel_list)
-    RunPct = list(squirrel_list.values_list('run').annotate(Count('run')))#[0]/len(squirrel_list)
-    ChasePct = list(squirrel_list.values_list('chase').annotate(Count('chase')))#[0]/len(squirrel_list)
-    ClimbPct = list(squirrel_list.values_list('climb').annotate(Count('climb')))#[0]/len(squirrel_list)
-    EatPct = list(squirrel_list.values_list('eat').annotate(Count('eat')))#[0]/len(squirrel_list)
-    ForagePct = list(squirrel_list.values_list('forage').annotate(Count('forage')))#[0]/len(squirrel_list)
+    total_number = len(squirrel_list)
+    S_1 = list(squirrel_list.values_list('run').annotate(Count('run')))
+    run_pct_false = S_1[0][1] / total_number
+    S_2 = list(squirrel_list.values_list('chase').annotate(Count('chase')))
+    chase_pct_false = S_2[0][1] / total_number
+    S_3 = list(squirrel_list.values_list('climb').annotate(Count('climb')))
+    climb_pct_false = S_3[0][1] / total_number
+    S_4 = list(squirrel_list.values_list('eat').annotate(Count('eat')))
+    eat_pct_false = S_4[0][1] / total_number
+    S_5 = list(squirrel_list.values_list('forage').annotate(Count('forage')))
+    forage_pct_false = S_5[0][1] / total_number
 
-    context = {'TotalNumber':TotalNumber,
-                'RunPct':RunPct,
-                'ChasePct':ChasePct,
-                'ClimbPct':ClimbPct,
-                'EatPct':EatPct,
-                'ForagePct':ForagePct,
+
+    context = {'TotalNumber':total_number,
+                'RunPct':run_pct_false,
+                'ChasePct':chase_pct_false,
+                'ClimbPct':climb_pct_false,
+                'EatPct':eat_pct_false,
+                'ForagePct':forage_pct_false,
     }
 
     return context
