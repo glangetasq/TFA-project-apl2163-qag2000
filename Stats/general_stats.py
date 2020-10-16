cfa3528ec7e16b477666c2673baa7e6f9868f574
Quentin Glangetas <qag2000@columbia.edu>
2020-10-16 18:10:45 -0400

    Slight modification of the stats view

--- Stats/general_stats.py
+++ Stats/general_stats.py
@@ -5,23 +5,23 @@ def general_stats(squirrel_list):
 
     total_number = len(squirrel_list)
     S_1 = list(squirrel_list.values_list('run').annotate(Count('run')))
-    run_pct_false = math.round(((S_1[0][1] / total_number)*100),2)
+    run_pct_true = math.round(((S_1[1][1] / total_number)*100),2)
     S_2 = list(squirrel_list.values_list('chase').annotate(Count('chase')))
-    chase_pct_false = math.round(((S_2[0][1] / total_number)*100), 2)
+    chase_pct_true = math.round(((S_2[1][1] / total_number)*100), 2)
     S_3 = list(squirrel_list.values_list('climb').annotate(Count('climb')))
-    climb_pct_false = math.round(((S_3[0][1] / total_number)*100), 2)
+    climb_pct_true = math.round(((S_3[1][1] / total_number)*100), 2)
     S_4 = list(squirrel_list.values_list('eat').annotate(Count('eat')))
-    eat_pct_false = math.round(((S_4[0][1] / total_number)*100), 2)
+    eat_pct_true = math.round(((S_4[1][1] / total_number)*100), 2)
     S_5 = list(squirrel_list.values_list('forage').annotate(Count('forage')))
-    forage_pct_false = math.round(((S_5[0][1] / total_number)*100), 2) 
+    forage_pct_true = math.round(((S_5[1][1] / total_number)*100), 2)
 
 
     context = {'TotalNumber':total_number,
-                'RunPct':run_pct_false,
-                'ChasePct':chase_pct_false,
-                'ClimbPct':climb_pct_false,
-                'EatPct':eat_pct_false,
-                'ForagePct':forage_pct_false,
+                'RunPct':run_pct_true,
+                'ChasePct':chase_pct_true,
+                'ClimbPct':climb_pct_true,
+                'EatPct':eat_pct_true,
+                'ForagePct':forage_pct_true,
     }
 
     return context
