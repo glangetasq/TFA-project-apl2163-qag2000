f3f4d9d42a430df7a95b22818cc5c0c47206fd04
Quentin Glangetas <qag2000@columbia.edu>
2020-10-16 14:42:08 -0400

    Updated launch.sh to python3. 3

--- launch.sh
+++ launch.sh
@@ -4,4 +4,4 @@ pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py import_squirrel_data Data/squirrel_sightings.csv
-sudo /home/qag2000/squirrel_tracker/env/bin/python manage.py runserver 0.0.0.0:80
+sudo /home/qag2000/tfa-project/env/bin/python manage.py runserver 0.0.0.0:80
