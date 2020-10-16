f1e88c329190d252e2d3029946a4e7797072a943
Quentin Glangetas <qag2000@columbia.edu>
2020-10-16 14:40:16 -0400

    Updated launch.sh to python3. 2

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 python3 -m venv env
 source env/bin/activate
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py import_squirrel_data Data/squirrel_sightings.csv
