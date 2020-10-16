6d79d8fbf6dfb637ecd93a1a9524b09f72fc86f7
Quentin Glangetas <qag2000@columbia.edu>
2020-10-16 14:39:06 -0400

    Updated launch.sh to python3.

--- launch.sh
+++ launch.sh
@@ -1,8 +1,7 @@
-python -m venv env
+python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py import_squirrel_data Data/squirrel_sightings.csv
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py import_squirrel_data Data/squirrel_sightings.csv
 sudo /home/qag2000/squirrel_tracker/env/bin/python manage.py runserver 0.0.0.0:80
-
